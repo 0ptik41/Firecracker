4086c443160e01f3fd07f4d9712978b5bf333750
TylersDurden <0ptik41@protonmail.com>
2021-03-13 00:53:08 -0500

    I think this works to count the number of loops using RDI.

--- idk.asm
+++ idk.asm
@@ -10,6 +10,7 @@ _start:
 	mov 	eax, 4
 	int	0x80
 	; Now Loop For Debugging the Debugger
+	add	rdi, 0x1
 	loop	_start
 
 _start.start:
@@ -20,5 +21,5 @@ _start.start:
 	int 	0x80
 
 section		.data
-msg	db 'Hey!Im Gonna sit here for a while if you dont mind...', 0xa
+msg	db 0xa,' Hey! Gonna sit here for a while if you dont mind...', 0xa
 len	equ $ - msg

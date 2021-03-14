29dc058a87a4cab09520c56582c9f2176f9c6113
TylersDurden <0ptik41@protonmail.com>
2021-03-14 11:34:58 -0400

    Need the exit syscall or it will segfault after printing!

--- src/hello.asm
+++ src/hello.asm
@@ -8,6 +8,10 @@ _start:
     int     0x80   
     mov     eax,1  
     int     0x80   
+    mov	    rax, 0x60
+    mov     dil, 0
+    int     0x80
+
 section     .data
 msg     db  'Hello, friend.',0xa  
 len     equ $ - msg
